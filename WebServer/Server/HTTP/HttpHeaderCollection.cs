77ac2e11e77f6705d7eaa3bf2ac41cb8730c8007
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-04-29 12:14:52 +0300

    craated directory structure and some basic classes

---
+++ WebServer/Server/HTTP/HttpHeaderCollection.cs
@@ -0,0 +1,48 @@
+﻿namespace WebServer.Server.HTTP
+{
+    using Contracts;
+    using System;
+    using System.Collections.Generic;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void Add(HttpHeader header)
+        {
+            if (headers.ContainsKey(header.Key) == false)
+            {
+                headers[header.Key] = header;
+            }
+            else
+            {
+                throw new ArgumentException($"Key {header.Key} already exists");
+            }
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return headers.ContainsKey(key);
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+            if (headers.ContainsKey(key))
+            {
+                return headers[key];
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, this.headers);
+        }
+    }
+}
