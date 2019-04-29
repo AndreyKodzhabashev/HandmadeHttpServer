e44fbc48a5cc814585546b67104bed9e1344aac6
AndreyKo <andrey.kodzhabashev@gmail.com>
2019-04-29 15:01:09 +0300

    HTTP methods added

---
+++ WebServer/Server/HTTP/HttpContest.cs
@@ -0,0 +1,14 @@
+﻿namespace WebServer.Server.HTTP
+{
+    using Contracts;
+
+    public class HttpContest : IHttpContext
+    {
+        public HttpContest(string requestStr)
+        {
+            this.Request = new HttpRequest(requestStr);
+        }
+
+        public HttpRequest Request { get; }
+    }
+}
